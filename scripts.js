e4e5aa749451a27dee9d995126db841b5e3d03b7
Quynh Luu <quynhtlluu@gmail.com>
2022-09-14 19:15:46 -0700

    WIP Add color randomizer

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+/*function displayNewColors() {
+    document.getElementById('cb1').style.backgroundColor = this.colors[1];
+}*/
+
+window.onload = function() {
+
+}
+
+
 var hexcode = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 class Color {
@@ -5,7 +14,7 @@ class Color {
     this.locked = false;
     }
     randomizeHexcode() {
-        this.hexcode = '';
+        this.hexcode = '#';
         for (var i = 0; i < 6; i++) {
             this.hexcode += hexcode[Math.floor(Math.random() * hexcode.length)];
         }
@@ -13,11 +22,35 @@ class Color {
     }
 }
 
+var newHexcode = new Color()
+
+
 class Palette {
-    
+    constructor() {
+    this.colors = []
+    this.paletteID = Date.now();
+    }
+    generateNewColors() {
+        this.colors = []
+        for (var i = 0; i < 5; i++) {
+            this.colors.push(newHexcode.randomizeHexcode());
+        } 
+        document.getElementById('cb1').style.backgroundColor = this.colors[1];
+  //  return this.displayNewColors
+    } 
 }
 
-var color1 = new Color();
+
+var palette1 = new Palette();
+
+/*Note: on page load, a new instance of Palette will be declared. This Palette will be used until a user decides to save that Palette. Then, a new instance will be created and used until that palette is saved. Continue ad infinitum.
+It should have 5 Colors
+It should have a unique ID
+It should be able to replace the Colors with new Colors
+It should be able to lock Colors
+It should only replace unlocked Colors*/
+
+
 
 // goal:
 // make a random assortment of strings
