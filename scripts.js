d9592758acfa9d9bdbb344c3b54f93cee7dfad02
Angie Staffieri <angie.staffieri@gmail.com>
2022-09-15 19:57:17 -0600

    Functional random color button and syntax cleanup in JS

--- scripts.js
+++ scripts.js
@@ -1,17 +1,12 @@
-/*function displayNewColors() {
-    document.getElementById('cb1').style.backgroundColor = this.colors[1];
-}*/
 
-var colorBox1 = document.getElementById('cb1')
-var colorBox2 = document.getElementById('cb2')
-var colorBox3 = document.getElementById('cb3')
-var colorBox4 = document.getElementById('cb4')
-var colorBox5 = document.getElementById('cb5')
+
+var colorBox1 = document.getElementById('cb1');
+var colorBox2 = document.getElementById('cb2');
+var colorBox3 = document.getElementById('cb3');
+var colorBox4 = document.getElementById('cb4');
+var colorBox5 = document.getElementById('cb5');
 var colorBoxes = [colorBox1, colorBox2, colorBox3, colorBox4, colorBox5]
-
-window.onload = function() {
-
-}
+var newPaletteButton = document.querySelector('button')
 
 
 var hexcode = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -27,29 +22,34 @@ class Color {
         }
         return this.hexcode
     }
-}
+};
 
-var newHexcode = new Color()
+var newHexcode = new Color();
 
 
 class Palette {
     constructor() {
     this.colors = []
     this.paletteID = Date.now();
-    console.log(this.paletteID)
     }
     generateNewColors() {
         this.colors = []
         for (var i = 0; i < 5; i++) {
-            if (!newHexcode[i].locked) {
+            // if (!newHexcode[i].locked) {
             this.colors.push(newHexcode.randomizeHexcode());
             colorBoxes[i].style.backgroundColor = this.colors[i]
-            }
-        } 
+            // }
+        }
     }
- //       document.getElementById('cb1').style.backgroundColor = this.colors[0];
-  //  return this.displayNewColors
-} 
+};
+
+var displayedPalette = new Palette();
+function newColorData() {
+  displayedPalette.generateNewColors()
+}
+window.addEventListener('load', displayedPalette.generateNewColors());
+newPaletteButton.addEventListener('click', newColorData);
+
 
 /*function lockColor() {
  if (this.locked === true) {
@@ -58,24 +58,3 @@ class Palette {
     //return unlocked image
  }
 } */
-
-
-
-
-/*Note: on page load, a new instance of Palette will be declared. This Palette will be used until a user decides to save that Palette. Then, a new instance will be created and used until that palette is saved. Continue ad infinitum.
-It should have 5 Colors
-It should have a unique ID
-It should be able to replace the Colors with new Colors
-It should be able to lock Colors
-It should only replace unlocked Colors*/
-
-
-
-// goal:
-// make a random assortment of strings
-// organize strings within an array
-//         make a variable to store array 
-
-// each string should have six characters (in total) from:
-// A-F
-// 0-9
